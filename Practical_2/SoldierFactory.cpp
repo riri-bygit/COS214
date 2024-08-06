0bb42fd5d9bfd32b560994a9d17aed2880ad45c4
riri-bygit <rethabileb13@gmail.com>
2024-08-06 22:26:43 +0200

    .

---
+++ Practical_2/SoldierFactory.cpp
@@ -0,0 +1,22 @@
+// virtual Soldiers* createUnit() = 0;
+
+    // int calculateTotalHealthPerUnit() {
+    //     Soldiers* unit = createUnit();
+    //     int totalHealth = unit->healthPerSoldier * unit->amountOfSoldiersPerUnit;
+    //     delete unit;  // Clean up after use
+    //     return totalHealth;
+    // }
+
+    // int calculateTotalDamagePerUnit() {
+    //     Soldiers* unit = createUnit();
+    //     int totalDamage = unit->damagePerSoldier * unit->amountOfSoldiersPerUnit;
+    //     delete unit;  // Clean up after use
+    //     return totalDamage;
+    // }
+
+    // int calculateTotalDefencePerUnit() {
+    //     Soldiers* unit = createUnit();
+    //     int totalDefence = unit->defencePerSoldier * unit->amountOfSoldiersPerUnit;
+    //     delete unit;  // Clean up after use
+    //     return totalDefence;
+    // }
--- Practical_2/SoldierFactory.h
+++ Practical_2/SoldierFactory.h
@@ -4,8 +4,6 @@
 #include <iostream>
 using namespace std;
 
-
-
 class SoldierFactory 
 {
 
@@ -14,29 +12,28 @@ private:
 
 public:
     virtual ~SoldierFactory();
-    // virtual ~SoldierFactory();
-
+    
 protected:
     virtual Soldiers* createUnit() = 0;
     virtual int calculateTotalHealthPerUnit();
     virtual int calculateTotalDamagePerUnit();
     virtual int calculateTotalDefencePerUnit();
 
-    // virtual Soldiers* createUnit() = 0;
+    virtual Soldiers* createUnit() = 0;
 
-    // int calculateTotalHealthPerUnit() {
-    //     Soldiers* unit = createUnit();
-    //     int totalHealth = unit->healthPerSoldier * unit->amountOfSoldiersPerUnit;
-    //     delete unit;  // Clean up after use
-    //     return totalHealth;
-    // }
+    int calculateTotalHealthPerUnit() {
+        Soldiers* unit = createUnit();
+        int totalHealth = unit->healthPerSoldier * unit->amountOfSoldiersPerUnit;
+        delete unit;  // Clean up after use
+        return totalHealth;
+    }
 
-    // int calculateTotalDamagePerUnit() {
-    //     Soldiers* unit = createUnit();
-    //     int totalDamage = unit->damagePerSoldier * unit->amountOfSoldiersPerUnit;
-    //     delete unit;  // Clean up after use
-    //     return totalDamage;
-    // }
+    int calculateTotalDamagePerUnit() {
+        Soldiers* unit = createUnit();
+        int totalDamage = unit->damagePerSoldier * unit->amountOfSoldiersPerUnit;
+        delete unit;  // Clean up after use
+        return totalDamage;
+    }
 
     // int calculateTotalDefencePerUnit() {
     //     Soldiers* unit = createUnit();
